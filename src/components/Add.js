744d5cd50638c41a0b810e9d28f9f9026ea0f505
Naoya Kawasaki <qqks23sd@gmail.com>
2020-11-23 18:51:57 +0900

    call save book api to regist book

--- src/components/Add.js
+++ src/components/Add.js
@@ -1,3 +1,4 @@
+import React, { useEffect, useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
@@ -11,26 +12,41 @@ const useStyles = makeStyles((theme) => ({
     },
 }));
 
-function regist(e) {
-
-}
-
 function Add() {
     const classes = useStyles();
+    const [ name, setName ] = useState([]);
+    const [ author, setAuthor ] = useState([]);
+    const [ url, setUrl ] = useState([]);
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        console.log("name:", name, "author", author, "url:", url);
+        fetch(process.env.REACT_APP_BACKEND_URL + "/api/books", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                "name": name,
+                "author": author,
+                "url": url
+            })
+        });
+    }
 
     return (
-        <form className={classes.root} noValidate autoComplete="off">
+        <form onSubmit={handleSubmit} className={classes.root} >
             <div>
-                <TextField id="name" label="Name" />
+                <TextField id="name" label="Name" required onChange={(e) => {setName(e.target.value)}} />
             </div>
             <div>
-                <TextField id="auther" label="Auther" />
+                <TextField id="author" label="Author" onChange={(e) => {setAuthor(e.target.value)}} />
             </div>
             <div>
-                <TextField id="url" label="URL" />
+                <TextField id="url" label="URL" onChange={(e) => {setUrl(e.target.value)}} />
             </div>
             <div>
-                <Button onClick={ regist } variant="contained" href="/Books">Register</Button>
+                <Button type="submit" variant="contained">Register</Button>
             </div>
         </form>
     );
